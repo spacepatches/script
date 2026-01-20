472e69651fbfa75fed31ecfd7141ab83eda53343
Space Patches <spacepatches.contact@gmail.com>
2026-01-20 09:21:15 +0100

    Update import_launch_loop_update_local.py

--- import_launch_loop_update_local.py
+++ import_launch_loop_update_local.py
@@ -12,11 +12,11 @@ from dotenv import load_dotenv
 load_dotenv()
 
 conn = psycopg2.connect(
-    host=os.getenv("SUPABASE_DB_HOST"),
-    dbname=os.getenv("SUPABASE_DB_NAME"),
-    user=os.getenv("SUPABASE_DB_USER"),
-    password=os.getenv("SUPABASE_DB_PASSWORD"),
-    port=os.getenv("SUPABASE_DB_PORT"),
+    host=os.getenv("DB_HOST"),
+    dbname=os.getenv("DB_NAME"),
+    user=os.getenv("DB_USER"),
+    password=os.getenv("DB_PASSWORD"),
+    port=os.getenv("DB_PORT"),
     connect_timeout=10
 )
 
